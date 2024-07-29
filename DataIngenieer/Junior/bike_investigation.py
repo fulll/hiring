a3501dc7d77cabe98ad7083e3123b96fee7c2188
jlecomte-IED <jeremie.lecomte@fulll.fr>
2024-07-29 18:10:45 +0200

    move python tests

--- DataIngenieer/Junior/bike_investigation.py
+++ DataIngenieer/Junior/bike_investigation.py
@@ -20,7 +20,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print("Hello! Let's explore some bikeshare data!")
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington). 
 
     # TO DO: get user input for month (all, january, february, ... , june)
 
